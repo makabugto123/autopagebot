08a6c753a42cef748d152fc2de22a290abb29eed
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-12-03 13:03:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 const VERIFY_TOKEN = 'hakdog';
-const INTERNAL_API_BASE = 'https://databasefbpage-bytd.onrender.com'; // Put your Own Database Url here use https://github.com/wedfhujkkmhhgg233/databasefbpage
+const INTERNAL_API_BASE = 'https://databasefbpage-r484.onrender.com'; // Put your Own Database Url here use https://github.com/wedfhujkkmhhgg233/databasefbpage
 
 // Edit Also the handlemessage Database Url
 app.get('/webhook', (req, res) => {
