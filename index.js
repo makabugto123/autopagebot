575b0b31d550788719a93639f3d212c38bd0fbfa
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-12-04 21:36:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 const VERIFY_TOKEN = 'hakdog';
-const INTERNAL_API_BASE = 'https://83f5-2407-3640-2155-2428-00-1.ngrok-free.app'; // Put your Own Database Url here use https://github.com/wedfhujkkmhhgg233/databasefbpage
+const INTERNAL_API_BASE = 'https://61e5-2407-3640-2155-2428-00-1.ngrok-free.app'; // Put your Own Database Url here use https://github.com/wedfhujkkmhhgg233/databasefbpage
 
 // Edit Also the handlemessage Database Url
 app.get('/webhook', (req, res) => {
