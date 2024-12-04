85ed601b1fdee70c071ee312c9f249e82174ee77
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-12-04 22:38:43 +0800

    Update handleMessage.js

--- handles/handleMessage.js
+++ handles/handleMessage.js
@@ -23,7 +23,7 @@ fs.readdirSync(path.join(__dirname, '../commands'))
   });
 
 // Internal API endpoints
-const INTERNAL_API_BASE = 'https://411a-2407-3640-2155-2428-00-1.ngrok-free.app'; //put your own database url here use https://github.com/wedfhujkkmhhgg233/databasefbpage
+const INTERNAL_API_BASE = 'https://207c-2605-a140-2155-2428-00-1.ngrok-free.app'; //put your own database url here use https://github.com/wedfhujkkmhhgg233/databasefbpage
 const ENDPOINT_FIND = `${INTERNAL_API_BASE}/find?json=`;
 
 /**
