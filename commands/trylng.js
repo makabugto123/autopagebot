df051cc2033794ce774d559a39e8f0c36e357892
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2024-12-07 23:08:20 +0800

    Update trylng.js

--- commands/trylng.js
+++ commands/trylng.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: "gpt4o",
+  name: "trylang",
   description: "interact with gpt4o pro with generate & recognize image",
   author: "Emman Co",
 
