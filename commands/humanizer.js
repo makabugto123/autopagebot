095cb82f4d0aca0d34536eeff4446c0840062b0b
makabugto123 <139425756+makabugto123@users.noreply.github.com>
2025-01-25 12:42:10 +0800

    Update humanizer.js

--- commands/humanizer.js
+++ commands/humanizer.js
@@ -3,7 +3,7 @@ const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
   name: 'humanizer',
-  description: 'Humanize the AI Generated Text',
+  description: 'Humanize the AI Generated Text With AI DETECTOR',
   usage: 'Humanizer [AI-generated text here]',
   author: 'coffee',
 
@@ -16,9 +16,18 @@ module.exports = {
        const response = await axios.get(`https://kaiz-apis.gleeze.com/api/humanizer?q=${encodeURIComponent(prompt)}`);
       const mess = response.data.response;
       
+      const response1 = await axios.get(`https://kaiz-apis.gleeze.com/api/aidetector?q=${encodeURIComponent(mess)}`);
+      const dat = response1.data.data;
+      const isHuman = dat.isHuman;
+      const aiWords = dat.aiWords;
+      const feedback = dat.feedback;
+      
+      const mess1 = `🤖 𝗛𝗨𝗠𝗔𝗡𝗜𝗭𝗘𝗥\n━━━━━━━━━━━━━━━━━━\nResponse:\n\n${mess}\n\n🤖 𝗔𝗜 𝗗𝗘𝗧𝗘𝗖𝗧𝗢𝗥\n━━━━━━━━━━━━━━━━━━\n💌𝙈𝙚𝙨𝙨𝙖𝙜𝙚: ${feedback}\n👭𝙄𝙎𝙃𝙐𝙈𝘼𝙉: ${isHuman}\n🤖𝗜𝗦 𝗔𝗜: ${aiWords}\n`
+      
+      
       const parts = [];
 
-            for (let i = 0; i < mess.length; i += 1999) {
+            for (let i = 0; i < mess1.length; i += 1999) {
                 parts.push(mess.substring(i, i + 1999));
             }
 
